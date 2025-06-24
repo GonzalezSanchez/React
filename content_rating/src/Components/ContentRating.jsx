eb46155fc7e8cecbd22724a0d177ddfba9c68ead
AGS <a_gonzalez_sanchez@hotmail.com>
2025-06-24 11:59:55 -0400

    the second lab of module 1 is finished - the lab content rating application to like or dislike content

--- content_rating/src/Components/ContentRating.jsx
+++ content_rating/src/Components/ContentRating.jsx
@@ -1,16 +1,48 @@
 
+//this is a class componnent
 import React, { Component } from 'react';
 import './ContentRating.css';
 
 class ContentRating extends Component {
   constructor() {
     super();
+    //Initialize the states for the likes and dislikes 
+    //count within the constructor
+    this.state = {
+        likes: 0,
+        dislikes: 0,
+        totalRatings: 0,
+        handleLike:() => {
+          this.setState((prevState) => ({
+            likes: prevState.likes + 1,
+            totalRatings: prevState.totalRatings + 1
+          }));
+        },
+        handleDislike:() => {
+          this.setState((prevState) => ({
+            dislikes: prevState.dislikes + 1,
+            totalRatings: prevState.totalRatings + 1
+          }));
+        }
+    }
   }
   render() {
     return (
-     <>
-     <h1>Text Content Rating</h1>
-     </>
+        <div className='content-rating'>
+            <p>
+            {/*Add text here*/}
+                Text
+            </p>
+            <div className='rating-buttons'>
+                <button className="like-button" onClick={this.state.handleLike}>
+                    Like ({this.state.likes})
+                </button>
+                <button className="dislike-button" onClick={this.state.handleDislike}>
+                    Dislike ({this.state.dislikes})
+                </button>
+            </div>
+            <p>Total Ratings: {this.state.totalRatings}</p>
+        </div>
     );
   }
 }
